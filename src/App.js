0305c6a3686ec05eb1f17723b4fee6bd7200eca1
Jeff Hoyt <JeffjHoyt@gmail.com>
2023-04-20 14:38:49 -0400

    css

--- src/App.js
+++ src/App.js
@@ -7,10 +7,9 @@ function App() {
     { text: "How are you doing today?", sender: "received" },
     { text: "I'm doing great, thanks for asking!", sender: "sent" },
     { text: "What have you been up to lately?", sender: "sent" },
-    { text: "Not much, just staying busy with work.", sender: "received" },
     {
-      text: "That sounds interesting. What kind of work do you do?",
-      sender: "sent",
+      text: "Not much, just staying busy with work. Any plans for the weekend?",
+      sender: "received",
     },
   ]);
 
@@ -31,15 +30,17 @@ function App() {
           </div>
         ))}
       </div>
-      <form onSubmit={handleMessageSubmit} className="input-container">
-        <input
-          type="text"
-          placeholder="Type a message..."
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-        />
-        <button type="submit">Send</button>
-      </form>
+      <div className="form">
+        <form onSubmit={handleMessageSubmit} className="input-container">
+          <input
+            type="text"
+            placeholder="Type a message..."
+            value={newMessage}
+            onChange={(e) => setNewMessage(e.target.value)}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </div>
   );
 }
